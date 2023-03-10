d425bdd4e5c7278204f5f83ea45002519b0c71f9
mariannadia <103584887+mariannadia@users.noreply.github.com>
2023-03-10 09:49:44 +0200

    Update TOP 10 USERS.sql

--- TOP 10 USERS.sql
+++ TOP 10 USERS.sql
@@ -17,11 +17,11 @@ from orders_per_user
  ),
 
  top_10 as 
- (select city,  count(user_id) as top_10_users, sum(orders_per_user_city) as top_10 from ranking
+ (select city,  count(user_id) as top_10_users, sum(orders_per_user_city) as order_top_10 from ranking
  where rank_ <= 10
  group by city)
  
-select a.city,top_10_users, top_10/total_orders as percentage_contribution
+select a.city,top_10_users, order_top_10/total_orders as percentage_contribution
 from total_city_orders a left join top_10 b on a.city = b.city
 order by top_10/total_orders desc
 
